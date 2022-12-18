4a9961239ab76b52ac59cf5a6a436a957f0147dc
Jesus Cristancho <jesuscriss301@gmail.com>
2022-12-18 18:52:03 -0500

    merge

---
+++ assets/README.md
@@ -0,0 +1,39 @@
+# Sejmet Whips's Store
+
+Sejmet Whip´s Store es un proyecto de emprendimiento que busca satisfacer las necesidades de un nicho específico, el mercado de látigos con fines eróticos. Para ello, se busca diseñar un e-commerce empleando tecnologias del desarrollo web para asi poder desplegar los productos que se ofrecen.
+
+Las tecnologias empleadas para el desarrollo de la capa frontend del proyecto son las siguientes:
+|Programming languages|Markup Language|Style LanguageDev|Frameworks|
+|---|---|---|---|
+|![JavaScript](https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E") |![HMTL5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) |![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white") |![bootstrap](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)|
+
+La arquitectura que se piensa utilizar para el desarrollo del proyecto es la arquitectura MVC, la cual consiste en Model - View - Controller:
+<br>
+![mvc](/assets/resources/1200px-ModelViewControllerDiagram_es.svg.png)
+
+El logo del proyecto es el siguientes:
+
+![logo](/assets/resources/logo.png)
+
+Se realizaron diseños mockups de lo que se espera desarrollar en el proyecto:
+
+Login
+![login](/assets/images/login.png)
+
+Create account
+![create-account](/assets/images/create%20acount.png)
+
+Home
+![home](/assets/images/Home.png)
+
+Products
+![products](/assets/images/Products.png)
+
+Details Product - Description
+![details-product-description](/assets/images/Details%20Product%20-%20Description.png)
+
+Details Product - Instructions
+![details-product-instructions](/assets/images/Details%20Product%20-%20Instructions.png)
+
+Shopping Cart
+![shopping-cart](/assets/images/Shoppin%20Cart.png)
--- js/catalogo.js
+++ js/catalogo.js
@@ -1,8 +1,10 @@
-window.onload = function(){
+fetchData();
+window.onload = function () {
     slideOne();
     slideTwo();
-    cargarProductos()
+    //cargarProductos()
 }
+
 let sliderOne = document.getElementById("slider-1");
 let sliderTwo = document.getElementById("slider-2");
 let displayValOne = document.getElementById("range1");
@@ -12,85 +14,62 @@ let sliderTrack = document.querySelector(".slider-track");
 let sliderMaxValue = document.getElementById("slider-1").max;
 
 let catalog = document.getElementById("catalogo");
-let cardproducto = document.getElementById("cardproducto");
-let imgproduct = document.getElementById("imgproduct");
-let nameP = document.getElementById("nombreP");
-let contenP = document.getElementById("contenidoP");
-let add = document.getElementById("add");
-let buy = document.getElementById("buy");
-let details = document.getElementById("detalles");
+const templateCard = document.getElementById('template-card').content;
+const fragment = document.createDocumentFragment();
 
-function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+function slideOne() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
         sliderOne.value = parseInt(sliderTwo.value) - minGap;
     }
     displayValOne.value = sliderOne.value;
     fillColor();
 }
-
-function slideTwo(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
+function slideTwo() {
+    if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
         sliderTwo.value = parseInt(sliderOne.value) + minGap;
     }
     displayValTwo.value = sliderTwo.value;
     fillColor();
 }
-
-function fillColor(){
+function fillColor() {
     percent1 = (sliderOne.value / sliderMaxValue) * 100;
     percent2 = (sliderTwo.value / sliderMaxValue) * 100;
     sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , crimson ${percent1}% , crimson ${percent2}%, #dadae5 ${percent2}%)`;
 }
 
-function cargarProductos() {
-    let imagen = "https://tuagro.com/wp-content/uploads/2019/06/IMG_20190611_102143.jpg";
-    let nombreStr="pegarico";
-    let contenido="si pega rico";
-    for(let i = 0; i < 9; i++){
-        let carta=cargarproducto(imagen,nombreStr,contenido);
-        
-    }
-} 
- function cargarproducto(imagen,nombreStr,contenido) {
-    let cartaP =cardproducto.cloneNode();
-        let imagenP=imgproduct.cloneNode();
-        let nombre= nameP.cloneNode();
-        let contenidoP=contenP.cloneNode();
-        let añadir = add.cloneNode();
-        let comprar = buy.cloneNode();
-        let detalles = details.cloneNode();
 
-        imagenP.setAttribute('src', imagen)
-        nombre.insertAdjacentText('beforeend', nombreStr);
-        contenidoP.insertAdjacentText('beforeend', contenido);
-        añadir.insertAdjacentText('beforeend', "Add to car");
-        comprar.insertAdjacentText('beforeend', "Buy now");
-        detalles.insertAdjacentText('beforeend', "see more");
-
-        cartaP.insertAdjacentElement('beforeend',imagenP);
-        cartaP.insertAdjacentElement('beforeend',nombre);
-        cartaP.insertAdjacentElement('beforeend',contenidoP);
-        cartaP.insertAdjacentElement('beforeend',añadir);
-        cartaP.insertAdjacentElement('beforeend',comprar);
-        cartaP.insertAdjacentElement('beforeend',detalles);
-
-    catalog.insertAdjacentElement('beforeend',cartaP);
+// Cargar datos de los productos
+function fetchData() {
+    fetch('http://localhost:8080/products')
+        .then(response => response.json())
+        .then(data => pintarCard(data));
 }
 
+// function fetchImages() {
+//     fetch('http://localhost:8080/images')
+//         .then(response => response.json())
+//         .then(images => images(images));
+// }
 
+// Pintar las cards de los productos 
+function pintarCard(data) {
+    data.forEach(item => {
+        let price = templateCard.getElementById("price");
+        let title = templateCard.getElementById("title");
+        let description = templateCard.getElementById("description");
 
-let categoriaone = document.getElementById("categoria1");
-let categoriaTwo = document.getElementById("categoria2");
-consulta();
+        templateCard.getElementById("add_cart").dataset.id = item.id;
 
-function consulta() {
-    fetch('http://localhost:8080/categories')
-      .then(response => response.json())
-      .then(data => construir(data));
-  
-  }
-  function construir(data){
-  
-    categoriaone.textContent = data[0].categoryName;
-    categoriaTwo.textContent = data[1].categoryName;
-  }
+        price.textContent = "$" + item.salePrice;
+        title.textContent = item.productName;
+        description.textContent = item.productDescription;
+
+        templateCard.querySelector('img').setAttribute('src', "/assets/images/products_images/" + 
+            (item.productName) + "/" + (item.productName) + " 1.jpg");
+
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    })
+    catalog.appendChild(fragment);
+}
+
