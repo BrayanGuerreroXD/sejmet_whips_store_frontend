af6287d68ddcdba4dcbeadcb232687e610dd9957
Jesus Cristancho <jesuscriss301@gmail.com>
2022-12-18 17:25:41 -0500

    agregar card

--- js/catalogo.js
+++ js/catalogo.js
@@ -12,6 +12,13 @@ let sliderTrack = document.querySelector(".slider-track");
 let sliderMaxValue = document.getElementById("slider-1").max;
 
 let catalog = document.getElementById("catalogo");
+let cardproducto = document.getElementById("cardproducto");
+let imgproduct = document.getElementById("imgproduct");
+let nameP = document.getElementById("nombreP");
+let contenP = document.getElementById("contenidoP");
+let add = document.getElementById("add");
+let buy = document.getElementById("buy");
+let details = document.getElementById("detalles");
 
 function slideOne(){
     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
@@ -20,6 +27,7 @@ function slideOne(){
     displayValOne.value = sliderOne.value;
     fillColor();
 }
+
 function slideTwo(){
     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
         sliderTwo.value = parseInt(sliderOne.value) + minGap;
@@ -27,6 +35,7 @@ function slideTwo(){
     displayValTwo.value = sliderTwo.value;
     fillColor();
 }
+
 function fillColor(){
     percent1 = (sliderOne.value / sliderMaxValue) * 100;
     percent2 = (sliderTwo.value / sliderMaxValue) * 100;
@@ -34,23 +43,42 @@ function fillColor(){
 }
 
 function cargarProductos() {
-    let innerHtml = '';
-    for(let i = 0; i < 30; i++){
-        innerHtml += `
-            <div class="card m-2 p-2" style="width: 14rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">APHRODISIA LEATHER WHIP</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <button class="btn mb-3" style="background-color: crimson; width: 100%;">Add to cart</button>
-                    <button class="btn mb-3" style="border-color: crimson; width: 100%">Buy now</button>
-                    <a href="#" style="color: crimson; width: 100%">Read reviews</a>
-                </div>
-            </div>
-        `;
+    let imagen = "https://tuagro.com/wp-content/uploads/2019/06/IMG_20190611_102143.jpg";
+    let nombreStr="pegarico";
+    let contenido="si pega rico";
+    for(let i = 0; i < 9; i++){
+        let carta=cargarproducto(imagen,nombreStr,contenido);
+        
     }
-    catalog.innerHTML = innerHtml;
-}   
+} 
+ function cargarproducto(imagen,nombreStr,contenido) {
+    let cartaP =cardproducto.cloneNode();
+        let imagenP=imgproduct.cloneNode();
+        let nombre= nameP.cloneNode();
+        let contenidoP=contenP.cloneNode();
+        let añadir = add.cloneNode();
+        let comprar = buy.cloneNode();
+        let detalles = details.cloneNode();
+
+        imagenP.setAttribute('src', imagen)
+        nombre.insertAdjacentText('beforeend', nombreStr);
+        contenidoP.insertAdjacentText('beforeend', contenido);
+        añadir.insertAdjacentText('beforeend', "Add to car");
+        comprar.insertAdjacentText('beforeend', "Buy now");
+        detalles.insertAdjacentText('beforeend', "see more");
+
+        cartaP.insertAdjacentElement('beforeend',imagenP);
+        cartaP.insertAdjacentElement('beforeend',nombre);
+        cartaP.insertAdjacentElement('beforeend',contenidoP);
+        cartaP.insertAdjacentElement('beforeend',añadir);
+        cartaP.insertAdjacentElement('beforeend',comprar);
+        cartaP.insertAdjacentElement('beforeend',detalles);
+
+    catalog.insertAdjacentElement('beforeend',cartaP);
+}
+
+
+
 let categoriaone = document.getElementById("categoria1");
 let categoriaTwo = document.getElementById("categoria2");
 consulta();
