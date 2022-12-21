8321da71bb94f46e9937a6c09183cbf1bfc40bda
Jesus Cristancho <jesuscriss301@gmail.com>
2022-12-20 20:44:39 -0500

    loggin

--- js/loggin.js
+++ js/loggin.js
@@ -1,31 +1,39 @@
-function push(data) {
-    fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(datos),
-            headers:{
-          "Content-Type":"application/json; charset=UTF-8"}
-          })
-      .then(response => response.json())
-      .then(json => console.log(json))
-}
-function consultapost(){
-    var c =document.querySelector("#buscarid")
-    var i = c.value
-    let datos={
-      userName: "pepito",
-      userCode: 115184222,
-      identifyCardNumber: 122445218,
-      email: i+"@ufps.edu.co",
-      password: "brayan123",
-      admin: true}
-
-    console.log(i);
-      fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(datos),
-            headers:{
-          "Content-Type":"application/json; charset=UTF-8"}
-          })
-      .then(response => response.json())
-      .then(json => console.log(json))
+let correo="";
+let pass="";
+ window.onload = function () {
+  if(localStorage.length>0){
+    correo =localStorage.getItem("email");
+    pass = localStorage.getItem("password");
+    consulta();
   }
+  
+
+}
+
+function consulta() {
+  if (correo.length==0 && pass.length==0) {
+    let email = document.getElementById('usuario');
+    let contraceña = document.getElementById('password');
+    correo = email.value;
+    pass = contraceña.value;
+  }  
+  console.log(correo + " -> "+ pass);
+  fetch("http://localhost:8080/users/findByEmail/"+correo)
+    .then(response => response.json())
+    .then(json => contra(json, pass));
+}
+
+function contra(json, contraceña) {
+ console.log(json);
+
+  if (json.password === contraceña) {
+    localStorage.setItem("email",json.email);
+    localStorage.setItem("password",json.password);
+    console.log(json.password+ " -> " + contraceña);
+    console.log("esta funcionando");
+    window.location.href = "catalog.html";
+  }else{
+    console.log("no funciona");
+    console.log(json.password + " -> " + contraceña);
+}
+}
